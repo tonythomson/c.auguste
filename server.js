310784e6090084b8b3fa20a87d0284a8889e7b69
Tony Thomson <thomson@me.com>
2013-03-10 11:59:46 -0700

    Adds server.js file to repo

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+      pg = require('pg'),
+       _ = require("underscore");
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(3000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/');
