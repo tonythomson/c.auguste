b741b68791ab603d16acabcd18c435c172a03c56
Tony Thomson <thomson@me.com>
2013-03-10 11:40:07 -0700

    Adds insertion of company CIKs and names from file into companies table. Also, comments & clean-up.

--- initDB.js
+++ initDB.js
@@ -10,6 +10,7 @@ var parseAccNum = function(path) {
 };
 
 var parseMasterIdxRow = function(row) {
+	// Takes a row from the IDX file and Returns a 'filing' object
 	var filing = {};
 	filing.cik = row[0];
 	filing.company_name = row[1];
@@ -19,17 +20,19 @@ var parseMasterIdxRow = function(row) {
 	return filing;
 };
 
-var removeDupes = function (filingsArray) {
-	filingsArray = _.uniq( _.map( filingsArray, function( filing ){
+var removeDupesFromArray = function (recordsArray) {
+	// Takes an array of objects and returns an array with duplicate objects removed
+	recordsArray = _.uniq( _.map( recordsArray, function( filing ){
 		return JSON.stringify( filing );
 	}));
-	filingsArray = _.map( filingsArray, function( filing ){
+	recordsArray = _.map( recordsArray, function( filing ){
 		return JSON.parse( filing );
 	});
-	return filingsArray;
+	return recordsArray;
 };
 
 var logFileResults = function(filename, linesSkipped, firstCount, lastCount){
+		// Logs some simple stats to console
 		console.log("Scanned file "+filename+":");
 		console.log("-"+linesSkipped+" non-delimited lines skipped");
 		console.log(lastCount + " rows scanned ("+(firstCount-lastCount) + " duplicates found/removed).");
@@ -38,6 +41,7 @@ var logFileResults = function(filename, linesSkipped, firstCount, lastCount){
 var conString = "pg://tony:@127.0.0.1/cauguste";
 var filename = "master.20130307.idx";
 var filingsArray = [];
+var companiesArray = [];
 var client = new pg.Client(conString);
 var linesSkipped = 0;
 
@@ -46,29 +50,37 @@ client.connect(function(err) {
 	csv()
 	.from.stream(fs.createReadStream(__dirname+'/data/'+filename), {delimiter:'|'})
 	.transform(function(data){
+		// Ignore rows in the file that are not delimited data
 		if((data.length < 5)||(data[0] === "CIK")) { linesSkipped++; return null; }
 		else { return data; }
 	})
 	.on('record', function(row,index){
+		// Build an array of filing objects based on parsed rows
 		filingsArray.push(parseMasterIdxRow(row));
 	})
 	.on('end', function(count){
 		var firstCount = filingsArray.length;
-		filingsArray = removeDupes(filingsArray);
+		filingsArray = removeDupesFromArray(filingsArray);
 		var lastCount = filingsArray.length;
 		logFileResults(filename, linesSkipped, firstCount, lastCount);
+
+		// Try and insert all unique filings from this file into the filings table (log & ignore INSERT errors);
 		_.each(filingsArray, function(filing, line) {
-			client.query('INSERT INTO filings (acc_num, form_type, file_date, issuer) VALUES ($1, $2, $3, $4)', [filing.acc_num, filing.form_type, filing.date, filing.cik], function(err, result) {
-				if (err) console.log("Error at "+filing.issuer+" / "+filing.acc_num+": "+err);
+			client.query('INSERT INTO filings (acc_num, form_type, file_date, issuer) VALUES ($1, $2, $3, $4)', [filing.acc_num, filing.form_type, filing.file_date, filing.cik], function(err, result) {
+				if (err) console.log("Error at "+ filing.cik + " / " + filing.acc_num + ": " + err);
 			});
+			companiesArray.push({cik: filing.cik, name: filing.company_name});
+		});
+
+		// Try and insert all unique companies from this file into the companies table (ignore INSERT errors);
+		companiesArray = removeDupesFromArray(companiesArray);
+		_.each(companiesArray, function(company){
+			client.query('INSERT INTO companies (cik, name) VALUES ($1, $2)', [company.cik, company.name], function(err, result) {});
 		});
 	})
 	.on('error', function(error){
 		console.log(error.message);
 	});
-
-
-
 
 });
 
